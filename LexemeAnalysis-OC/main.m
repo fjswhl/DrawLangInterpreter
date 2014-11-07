367bc4a56331360e253321be15301f06c1a86388
fjswhl <huanglexus@gmail.com>
2014-11-07 22:05:03 +0800

    Initial Commit

---
+++ LexemeAnalysis-OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LexemeAnalysis-OC
+//
+//  Created by Lin on 14/11/7.
+//  Copyright (c) 2014年 Lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
